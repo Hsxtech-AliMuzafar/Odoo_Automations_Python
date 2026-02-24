e2ad19db363868732211d28f216a18ebdcf13e2a
Hsxtech-AliMuzafar <aliman1351906@gmail.com>
2026-02-24 10:44:41 +0500

    [MOD] Intercompany Invoices

--- Invoice_Auto_Confirm_PIDistribution.py
+++ Invoice_Auto_Confirm_PIDistribution.py
@@ -14,17 +14,17 @@ if record:
             # 3. Check if it is an Invoice or Bill
             if record.move_type in ['out_invoice', 'out_refund', 'in_invoice', 'in_refund']:
                 
-                # 4. Partner specific tax assignment
-                if record.partner_id.id in [6598, 15]:
-                    for line in record.invoice_line_ids:
+                # 4. Tax logic: Remove for specific product or apply based on partner
+                for line in record.invoice_line_ids:
+                    if line.product_id.id == 19954:
+                        # Remove all taxes for product ID 19954
+                        line.write({'tax_ids': [(5, 0, 0)]})
+                    elif record.partner_id.id in [6598, 15]:
                         # Using .write() to avoid 'forbidden opcode: STORE_ATTR'
                         line.write({'tax_ids': [(4, 312)]})
-                else:
-                    # 5. For other partners, check for empty tax lines and add tax 302
-                    for line in record.invoice_line_ids:
-                        if not line.tax_ids:
-                            # Using .write() to avoid 'forbidden opcode: STORE_ATTR'
-                            line.write({'tax_ids': [(4, 302)]})
+                    elif not line.tax_ids:
+                        # For other partners, add tax 302 if empty
+                        line.write({'tax_ids': [(4, 302)]})
                 
-                # 6. Post the Invoice
+                # 5. Post the Invoice
                 record.action_post()
